0dfb14c91f6e6eda9a467a6bf47bd928576aa30c
AndiEcker <aecker2@gmail.com>
2020-10-15 18:41:44 +0100

    backup changes:
    - added metrics.py.

---
+++ metrics.py
@@ -0,0 +1,9 @@
+""" kivy metrics
+
+device                  dp(1000)    sp(1000)     font_size
+----------------------------------------------------------
+Samsung tablet              2000        2660            54
+Xaomi RedMi Note 7          2750        3163            78
+Dell Inspiron laptop        1000        1000            24
+
+"""
